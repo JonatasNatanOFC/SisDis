05b28f2c0a69d9490ee73c9b8cc45426dc6e0b83
igor721 <igorgabriel721@gmail.com>
2026-04-24 22:09:15 -0300

    Atualizando Host e preparando ambiente para testes com clientes dockerizados

--- src/main/java/ClienteAplicacao.java
+++ src/main/java/ClienteAplicacao.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 
 public class ClienteAplicacao {
 //10.10.130.44
-    private static final String HOST = "localhost";
+    private static final String HOST = "172.17.0.1";
     private static final int PORTA = 5000;
 
     private static String cortar(String texto, int tamanho) {
