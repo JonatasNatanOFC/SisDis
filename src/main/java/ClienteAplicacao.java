8af8127798bb269730aad31625d5567d1ef5b66d
igor721 <igorgabriel721@gmail.com>
2026-04-24 21:57:32 -0300

    Atualizando Host para testes com containers (Docker)

--- src/main/java/ClienteAplicacao.java
+++ src/main/java/ClienteAplicacao.java
@@ -9,8 +9,8 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class ClienteAplicacao {
-
-    private static final String HOST = "127.0.0.1";
+//10.10.130.44
+    private static final String HOST = "localhost";
     private static final int PORTA = 5000;
 
     private static String cortar(String texto, int tamanho) {
